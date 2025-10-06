31a13834d3b10444cf6379a2bd7717a6f83c9d2f
irismelendez <iris.13melendez@gmail.com>
2025-10-06 14:36:50 -0500

    Used the exception handling to run my code.

--- src/main/java/FamousQuotes.java
+++ src/main/java/FamousQuotes.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FamousQuotes {
+
+    public static void main(String[] args) {
+
+        //creating an array of strings to store 3 quotes.
+
+        String[] quotes = {"Everything happens for a reason.", "Life is short. Smile while you still have teeth.", "Stick to the plan, not your mood."};
+        //prompt the user to select between 1 or 3, and I have to make it display the quote
+
+        Scanner myScanner = new Scanner(System.in);
+
+        boolean valid = false;
+        do {
+            System.out.print("Select a quote(1, 2, or 3): ");
+            int choice = myScanner.nextInt();
+
+            try {
+                System.out.println(quotes[choice - 1]);
+                valid = true;
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println(ex);
+                valid = false;
+            }
+
+        } while (!valid);
+        myScanner.close();
+    }
 }
